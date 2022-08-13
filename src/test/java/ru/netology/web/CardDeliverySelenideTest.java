e5908ea0ae03f8a0b2d1a1a16cd16e60b7937763
zolfjk <zolfjkible@gmail.com>
2022-08-13 14:15:49 +0300

    appveyor changed

--- src/test/java/ru/netology/web/CardDeliverySelenideTest.java
+++ src/test/java/ru/netology/web/CardDeliverySelenideTest.java
@@ -2,16 +2,12 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Date;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliverySelenideTest {
     @Test
@@ -19,21 +15,22 @@ public class CardDeliverySelenideTest {
 
         Configuration.holdBrowserOpen = true;
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-        Date date = new Date();
-        Date myNewDate = DateUtils.addDays(date, 3);
+        LocalDateService localDate = new LocalDateService();
+        String planningDate = localDate.generateDate(4);
 
 
         open("http://localhost:9999");
         $("[data-test-id='city'] input").val("Нижний Новгород");
         $("[data-test-id='date'] input").doubleClick();
         $("[data-test-id='date'] input").sendKeys(Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(sdf.format(myNewDate));
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").val("Михаил Римский-Корсаков");
         $("[data-test-id='phone'] input").val("+79999999999");
         $("[data-test-id='agreement'] span[role]").click();
-        $("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $(" [data-test-id='notification']").should(Condition.visible, Duration.ofSeconds(15));
+        $x(".//*[text()='Забронировать']/parent::span/parent::button").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
 
     }
